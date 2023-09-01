074607fb681e9ff8f4b026a45cd535876468d4c5
Елизавета Земцова <elizabet9668@mail.ru>
2023-09-01 20:43:57 +0300

    внесение изменений добавление пермишена

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -2,14 +2,14 @@ from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
 from django.shortcuts import get_object_or_404
 from api.serializers import CommentSerializer
-from .permissions import AuthorOrReadOnly
+from .permissions import AuthorOrReadOnly, Moderator, Administrator
 from reviews.models import Review
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     pagination_class = PageNumberPagination
-    permission_classes = (AuthorOrReadOnly)
+    permission_classes = [AuthorOrReadOnly, Moderator, Administrator]
 
     def get_review(self):
         return get_object_or_404(
