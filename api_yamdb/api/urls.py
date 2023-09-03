5d82b4d4771d5ddaa9b8419d95961b7502af1bab
mortodello <129932790+mortodello@users.noreply.github.com>
2023-09-03 14:21:36 +0300

    Update urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from api.views import ReviewViewSet, CommentViewSet
 
 router_v1 = routers.DefaultRouter()
-router_v1.register(r'v1/reviews', ReviewViewSet)
+router_v1.register(r'v1/titles/(?P<title_id>\d+)/reviews', ReviewViewSet)
 router_v1.register(r'v1/titles/{title_id}/reviews/{review_id}/comments/',
                    CommentViewSet, basename='comment')
 
